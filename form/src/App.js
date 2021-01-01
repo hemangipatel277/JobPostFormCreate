43a13a1b6b4d67df3c888a9596ad96674feedf0a
Hemangi Patel <hemangipatel277@gmail.com>
2021-01-01 21:39:50 +0530

    changes in css

--- form/src/App.js
+++ form/src/App.js
@@ -1,106 +1,11 @@
 import React, { Component } from "react";
 import './App.css';
 
-class App extends React.Component { 
-
-//   constructor() {
-//       super();
-//       this.state = {
-//         fields: {},
-//         errors: {}
-//       }
-  
-//   this.handleChange = this.handleChange.bind(this);
-//   this.postJobForm = this.postJobForm.bind(this);
-//     };
-//     handleChange(e) {
-//        let fields = this.state.fields;
-//        fields[e.target.name] = e.target.value;
-//        this.setState ({
-//          fields
-//        });
-//     }
-// postJobForm(e){
-//   e.preventDefault();
-//   if (this.validateForm()) {
-//     let fields ={};
-//     fields["jobTitle"] = "";
-//     fields["location"] = "";
-//     fields["yearsOfExperience"] = "";
-//     fields["jobDescription"] = "";
-//     fields["category"] = "";
-//     fields["functionalArea"] = "";
-//     fields["graduatingYrea"] = "";
-//     fields["tag"] = "";
-//     this.setState({fields:fields});
-//     alert("Form submitted");
-//      }
-// }
-// validateForm(){
-//   let fields = this.state.fields;
-//       let errors = {};
-//       let formIsValid = true;
-
-//       if (!fields["jobTitle"]) {
-//         formIsValid = false;
-//         errors["jobTitle"] = "*Please enter your jobTitle.";
-//       }
-
-//       if (typeof fields["jobTitle"] !== "undefined") {
-//         if (!fields["jobTitle"].match(/^[a-zA-Z ]*$/)) {
-//           formIsValid = false;
-//           errors["jobTitle"] = "*Please enter alphabet characters only.";
-//         }
-//       }
-
-//       if (!fields["emailid"]) { formIsValid = false;
-//         errors["emailid"] = "*Please enter your email-ID.";
-//       }
-
-//       if (typeof fields["emailid"] !== "undefined") {
-//         //regular expression for email validation
-//         var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
-//         if (!pattern.test(fields["emailid"])) {
-//           formIsValid = false;
-//           errors["emailid"] = "*Please enter valid email-ID.";
-//         }
-//       }
-
-//       if (!fields["mobileno"]) {
-//         formIsValid = false;
-//         errors["mobileno"] = "*Please enter your mobile no.";
-//       }
-
-//       if (typeof fields["mobileno"] !== "undefined") {
-//         if (!fields["mobileno"].match(/^[0-9]{10}$/)) {
-//           formIsValid = false;
-//           errors["mobileno"] = "*Please enter valid mobile no.";
-//         }
-//       }
-//       if (!fields["password"]) {
-//         formIsValid = false;
-//         errors["password"] = "*Please enter your password.";
-//       }
-
-//       if (typeof fields["password"] !== "undefined") {
-//         if (!fields["password"].match(/^.*(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%&]).*$/)) {
-//           formIsValid = false;
-//           errors["password"] = "*Please enter secure and strong password.";
-//         }
-//       }
-
-//       this.setState({
-//         errors: errors
-//       });
-//       return formIsValid;
-
-
-//     }
-// }
+class App extends Component { 
   render()  {
     return (
-    <div className="Wrapper">
-      <div className="form-Wrapper">
+    <div className="App">
+      {/* <div className="form-Wrapper"> */}
         <h2>Post Job</h2>
         <h1 style={{ color: "#239276" }}> Basic Details </h1>
         <form onSubmit={this.handlesubmit} noValidate>
@@ -115,13 +20,13 @@ class App extends React.Component {
           </div>
 
           <div className="location">
-            <label htmlfor="select">Location*
-            <select multiple={true} value={this.state.value} onChange={this.handleChange}>
+            <label htmlfor="location">Location*
+            {/* <select multiple={true} value={this.state.value} onChange={this.handleChange}>
                 <option value="pune">Pune</option>
                 <option value="banglore">Banglore</option>
                 <option value="ahmedabad">Ahmedabad</option>
                 <option value="hyderabad">Hyderabad</option>
-            </select>
+            </select> */}
             </label><br></br>
             <input type="text"
               classname=""
@@ -143,14 +48,17 @@ class App extends React.Component {
           
           <div className="jobDescription">
             <label htmlfor="jobDescrition">Job Description*</label><br></br>
-            <input type="text"
+            {/* <input type="text"
               classname=""
               placeholder="Describe the role amd responsibility,skills required for the job and help the candidates understand the role better"
               name="jobDescrition"
               noValidate
-              onChange={this.handleChange}></input>
+              onChange={this.handleChange}></input> */}
+              <textarea id="jobDescription" name="jobDescription" placeholder=" Describe the role and responsibility,skills required for the job amd help the candidates understandthe role betters">
+              </textarea>
           </div>
         </form>
+
         <h1 style={{ color: "#239276"}}> Targeting </h1>
         <form>
           <div className="category">
@@ -183,10 +91,34 @@ class App extends React.Component {
          
         </form>    
       </div>
-    </div>
+    // </div>
       
     );
   }
+  // render() {
+  //   return (
+  //     <div className="App">
+  //     <p>Contact Me</p>
+  //     <div>
+  //     <form action="/action_page.php">
+  //     <label>First Name</label>
+  //     <input type="text" id="fname" name="firstname" placeholder="Your name.." />
+  //     <label>Last Name</label>
+  //     <input type="text" id="lname" name="lastname" placeholder="Your last name.." />
+  
+  
+  //     <label>Email</label>
+  //     <input type="email" id="email" name="email" placeholder="Your email" />
+      
+  
+  //     <label>Subject</label>
+  //     <textarea id="subject" name="subject" placeholder="Write something.."></textarea>
+  //     <input type="submit" value="Submit" />
+  //     </form>
+  //     </div>
+  //     </div>
+  //   );
+  // }
 }
 
 export default App;
